755ecc74e215cfc2502db5b2ed696f692a56484b
hadekunle <shitoheeb@gmail.com>
2022-10-30 16:55:16 -0500

    terraform

---
+++ 36.terraform_commands/main.tf
@@ -0,0 +1,24 @@
+
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# Make sure that permissions are enabled for the file being accessed 
+
+# Check downloaded providers in logins 
