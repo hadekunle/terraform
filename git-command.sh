b6b8598baf5fe770419d0ac214fa0e4a32ff15b4
hadekunle <shitoheeb@gmail.com>
2022-10-29 19:19:28 -0500

    terraform

--- git-command.sh
+++
@@ -1,9 +0,0 @@
-
-
-git add /Users/user/Desktop/AWS-COURSE/terraform_kode_kloud_github/tf/
-git commit -m "terraform"
-git push
-
-# git reset --soft HEAD^1 
-# git rm --cached -r .terraform 
-# git commit
