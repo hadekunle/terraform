b40a840016976c626d154eb21aa86fc5924422ad
hadekunle <shitoheeb@gmail.com>
2022-11-19 16:59:35 -0600

    terraform

---
+++ 84.terraform_modules/main.tf
@@ -0,0 +1,10 @@
+
+
+module "iam_iam-user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "3.4.0"
+  # insert the 1 required variable here
+    name = "max"
+  create_iam_access_key = "false"
+  create_iam_user_login_profile = "false"
+}
