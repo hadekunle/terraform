f53ed39ff0f542888b25da1c78e232463778d7ff
hadekunle <shitoheeb@gmail.com>
2022-11-19 23:41:27 -0600

    terraform

--- 87.functions_and_conditional_expressions/main.tf
+++ 87.functions_and_conditional_expressions/main.tf
@@ -8,3 +8,19 @@ resource "aws_iam_user" "users" {
   name = split(":",var.cloud_users)[count.index]
   count = length(split(":",var.cloud_users))
 }
+
+
+resource "aws_s3_bucket" "sonic_media" {
+     bucket = var.bucket
+  
+}
+
+resource "aws_s3_bucket_object" "upload_sonic_media" {
+     bucket = aws_s3_bucket.sonic_media.id
+     for_each = var.media
+     key = substr(each.value,7,14)
+     source = each.value
+    #  content = 
+     
+  
+}
